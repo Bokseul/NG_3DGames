a1f7857e3c1c99ef134e153f72cebfeb86b79cef
Bokseul <63533668+Bokseul@users.noreply.github.com>
2020-11-17 12:01:01 +0900

    [종훈] 스킬버튼
    - 스킬 버튼 클릭하면 스킬이 나감
    - 캐릭터 애니메이션 처리

--- Assets/Scripts/UI/PanelSkillUI.cs
+++ Assets/Scripts/UI/PanelSkillUI.cs
@@ -3,91 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PanelSkillUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class PanelSkillUI : MonoBehaviour
 {
-    private GameObject mObj_Ice;
     public int mBtn_num;
     public bool mBtn_skill;
 
     // atk = 0 | skill_fire = 1 | skill_ice = 2 | skill_heal = 3
 
-    public bool mAttack_UiBtn;
-    public bool mSkill_UiBtn;
-    public bool mHeal_UiBtn;
+    public bool mAttack_Btn = false;
+    public bool mFireSkill_Btn = false;
+    public bool mIceSkill_Btn = false;
+    public bool mHeal_UiBtn = false;
 
-    public bool mIceSkill_btn = false;
-
-    private void Awake()
-    {
-        mObj_Ice = GameObject.Find("Skill_Icon_Ice");
-
-        mBtn_num = 0;
-        mBtn_skill = false;
-
-        mAttack_UiBtn = false;
-        mSkill_UiBtn = false;
-        mHeal_UiBtn = false;
-    }
     
     private void Update()
     {
-        if (mObj_Ice)
+        if (mAttack_Btn)
         {
-            IceSkill_Btn(mIceSkill_btn);
+            UIEventToInGame.Instance.OnEventAttackBtn(mAttack_Btn);
         }
-        if (mBtn_skill)
+        else if (mIceSkill_Btn)
         {
-            GetSkillInputInfo(out mBtn_num);
-            OnClickSkill_btn(mBtn_num);
+            UIEventToInGame.Instance.OnEventIceSkillBtn(mIceSkill_Btn);
+        }
+        else if (mFireSkill_Btn)
+        {
+            UIEventToInGame.Instance.OnEventFireSkillBtn(mFireSkill_Btn);
+        }
+        else if (mHeal_UiBtn)
+        {
+            UIEventToInGame.Instance.OnEventHealSkillBtn(mHeal_UiBtn);
         }
     }
-
-    private void GetSkillInputInfo(out int mBtn_num)
-    {
-        mBtn_num = 1;
-    }
-
-    private void OnClickSkill_btn(int mBtn_num)
-    {
-        UIEventToInGame.Instance.OnEventSkillBtn(mBtn_num);
-    }
-
-    private void IceSkill_Btn(bool mIceSkill_btn)
-    {
-        mIceSkill_btn = true;
-    }
-
-    private void Update_IceSkill(PointerEventData eventData = null)
-    {
-
-    }
-
+    
     public void EventAttackBtn()
     {
-        mAttack_UiBtn = true;
-        Debug.Log(mAttack_UiBtn);
+        mAttack_Btn = true;
     }
-
-    public void EventSkillBtn()
+    public void EventSkill_Icebtn()
     {
-        mSkill_UiBtn = true;
+        mIceSkill_Btn = true;
     }
-
+    public void EventSkill_FireBtn()
+    {
+        mFireSkill_Btn = true;
+    }
     public void EventHealBtn()
     {
         mHeal_UiBtn = true;
     }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        UIEventToInGame.Instance.OnEventIceSkillBtn(mIceSkill_btn);
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        mSkill_UiBtn = false;
-        UIEventToInGame.Instance.OnEventBtnUp();
-    }
-
-    
 }
