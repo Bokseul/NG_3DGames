d30e87acb9dc0866c0363988be7ed47bc6d5cc04
Bokseul <63533668+Bokseul@users.noreply.github.com>
2020-11-19 18:32:28 +0900

    [종훈] Ad & Option
    - 광고 연결될수 있는 UI제작
    - 상점 연결될수 있는 UI제작

---
+++ Assets/Scripts/UI/PanelOption.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PanelOption : MonoBehaviour, IPointerDownHandler
+{
+    private bool RoyalShop_ck;
+
+    private GameObject Inventory;
+    private GameObject Quest;
+    private GameObject RoyalShop;
+
+    private void Awake()
+    {
+        Inventory = GameObject.Find("Op_Inventory");
+        Quest = GameObject.Find("Op_Quest");
+        RoyalShop = GameObject.Find("Op_RoyalShop");
+    }
+
+    private void Open_Inventory()
+    {
+
+    }
+
+    private void Open_Quest()
+    {
+
+    }
+
+    private void Open_RoyalShop()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (Inventory)
+            Open_Inventory();
+        else if (Quest)
+            Open_Quest();
+        else if (RoyalShop)
+            Open_RoyalShop();
+    }
+}
